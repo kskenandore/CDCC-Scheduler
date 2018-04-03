3393c92b21b65614cad6e18fb69d0266d0078b43
Keira Skenandore <33471219+kskenandore@users.noreply.github.com>
2018-04-03 18:22:26 -0500

    Uploading empty files
    These empty files show the basic structure of the site (which may change as development continues) and can be pulled to start development.

---
+++ private/shared/login_footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: keira
+ * Date: 4/3/2018
+ * Time: 6:17 PM
+ */
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php require_once('../private/initialize.php'); ?>
+<?php include('../private/shared/header.php'); ?>
+
+<?php include('../private/shared/footer.php'); ?>
