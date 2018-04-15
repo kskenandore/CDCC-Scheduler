8e54cf69c9c609906c193648f69c09e8a144aca5
chasonmj <37314758+chasonmj@users.noreply.github.com>
2018-04-15 07:59:43 -0500

    ListReservations
    - update reservations index.php to list all reservations

--- public/reservations/index.php
+++ public/reservations/index.php
@@ -10,13 +10,40 @@
 
         <table class="clear">
             <tr>
-                <th>Primary Key</th>
-                <th>Attribute</th>
-                <th>Attribute</th>
-                <th>Attribute</th>
-                <th>Attribute</th>
+                <th>Reservation ID</th>
+                <th>Customer</th>
+                <th>Venue</th>
+                <th>Date</th>
+                <th>Start Time</th>
                 <th>Actions</th>
             </tr>
+            <?php
+              // SQL to retrieve database records with formatted date result
+            	$sql = "SELECT a.reservation_id, concat(c.last_name, ', ', c.first_name  ) cname, v.name vname, DATE_FORMAT(`start_timestamp`, \"%c-%d-%Y\") rdate, DATE_FORMAT(`start_timestamp`, \"%l:%i %p\") rtime
+                      from reservations a
+                      inner join customers c
+                      on a.customer_id = c.customer_id
+                      inner join venues v
+                      on a.venue_id = v.venue_id
+                      order by a.reservation_id";
+
+            	// Execute SQL and save result
+            	$result = mysqli_query($dbc, $sql);
+
+              // Loop through each row returned by datbase
+              while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                echo '<tr>';
+                echo '<td>' . $row['reservation_id'] . '</td>';
+                echo '<td>' . $row['cname'] . '</td>';
+                echo '<td>' . $row['vname'] . '</td>';
+                echo '<td>' . $row['rdate'] . '</td>';
+                echo '<td>' . $row['rtime'] . '</td>';
+                echo '<td><a href="edit.php" class="button">Edit</a> <a href="cancel.php" class="button">Cancel</a></td>';
+                echo '</tr>';
+              }
+
+             ?>
+            <!-- Comment for future reference
             <tr>
                 <td>Example Data</td>
                 <td>Example Data</td>
@@ -25,30 +52,7 @@
                 <td>Example Data</td>
                 <td><a href="edit.php" class="button">Edit</a> <a href="cancel.php" class="button">Cancel</a></td>
             </tr>
-            <tr>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td><a href="edit.php" class="button">Edit</a> <a href="cancel.php" class="button">Cancel</a></td>
-            </tr>
-            <tr>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td><a href="edit.php" class="button">Edit</a> <a href="cancel.php" class="button">Cancel</a></td>
-            </tr>
-            <tr>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td>Example Data</td>
-                <td><a href="edit.php" class="button">Edit</a> <a href="cancel.php" class="button">Cancel</a></td>
-            </tr>
+          -->
         </table>
     </div>
 
