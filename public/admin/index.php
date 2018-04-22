a7d1c32f72c36b7828e2ceeade1e1e34ca0cc293
Keira <keiraskenandore@gmail.com>
2018-04-21 20:56:43 -0500

    Changed caterers to user admin

---
+++ public/admin/index.php
@@ -0,0 +1,55 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php $page_title = 'Admin'; ?>
+<?php include('../../private/shared/header.php'); ?>
+
+    <div id="content" class="clear">
+        <h2>Current Employees</h2>
+        <a href="new.php" class="button">+ New</a>
+        <input type="search" placeholder="search..." name="search"/>
+
+        <table class="clear">
+            <tr>
+                <th>Primary Key</th>
+                <th>Attribute</th>
+                <th>Attribute</th>
+                <th>Attribute</th>
+                <th>Attribute</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td><a href="edit.php" class="button">Edit</a> <a href="delete.php" class="button">Delete</a></td>
+            </tr>
+            <tr>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td><a href="edit.php" class="button">Edit</a> <a href="delete.php" class="button">Delete</a></td>
+            </tr>
+            <tr>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td><a href="edit.php" class="button">Edit</a> <a href="delete.php" class="button">Delete</a></td>
+            </tr>
+            <tr>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td>Example Data</td>
+                <td><a href="edit.php" class="button">Edit</a> <a href="delete.php" class="button">Delete</a></td>
+            </tr>
+        </table>
+    </div>
+
+<?php include('../../private/shared/footer.php'); ?>
